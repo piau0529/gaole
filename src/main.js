a565348aeaeaac703420b5eb886084806c1a350a
piau0529 <n10423303@yuntech.org.tw>
2022-09-05 17:50:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const FormAutoFill = new Vue({
   data: {
 
     // Google Apps Script 部署為網路應用程式後的 URL
-    gas: 'https://script.google.com/macros/s/AKfycbwyY7oLpoZ4lbcIwIWniLZ8U_pY8e7YBK9fakeszgR_77S2u1SV/exec',
+    gas: 'https://script.google.com/macros/s/AKfycbywcGT9NBssKaBqS5uLH0487IV_thmViXNz2puqDT-N7-R5w-LJuo3zVqYKUbA2H1ph/exec',
 
     id: '',
 
@@ -14,16 +14,17 @@ const FormAutoFill = new Vue({
     person: {},
 
     // Google Form 的 action URL
-    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeNBnd-yVJ7_-tMq5xaQrvt0j18UtabCFBTM0Eu2O3ivDecuQ/formResponse',
+    formAction: 'https://docs.google.com/forms/u/2/d/e/1FAIpQLScqs1amEuSipFEO036Rdv7hdYr18wvuOi9unYXOYv3XqUjanA/formResponse',
     
     // Google Form 各個 input 的 name
     input: {
-      id: 'entry.1815052017',
-      name: 'entry.1543576845',
-      gender: 'entry.892230025',
-      phone: 'entry.1003128242',
-      site: 'entry.260985931',
-      msg: 'entry.1782841550'
+      id: 'entry.2111259551',
+      email: 'entry.1788398348',
+      nick: 'entry.34866250',
+      msg1: 'entry.211211733',
+      msg2: 'entry.71595443',
+      msg3: 'entry.986226888',
+      msg4: 'entry.787660417'
     },
 
     // loading 效果要不要顯示
@@ -32,15 +33,15 @@ const FormAutoFill = new Vue({
   methods: {
     // ID 限填 4 碼
     limitIdLen(val) {
-      if(val.length > 4) {
-        return this.id =  this.id.slice(0, 4);
+      if(val.length > 11) {
+        return this.id =  this.id.slice(0, 10);
       }
     },
     // 送出表單
     submit() {
       // 再一次判斷是不是可以送出資料
-      if(this.person.name !== undefined) {
-        let params = `${this.input.id}=${this.person.id}&${this.input.name}=${this.person.name}&${this.input.gender}=${this.person.gender}&${this.input.phone}=${this.person.phone}&${this.input.site}=${this.person.site}&${this.input.msg}=${this.person.message || '無'}`;
+      if(this.person.nick !== undefined) {
+        let params = `${this.input.id}=${this.person.id}&${this.input.email}=${this.person.email}&${this.input.nick}=${this.person.nick}&${this.input.msg1}=${this.person.msg1}&${this.input.msg2}=${this.person.msg2}&${this.input.msg3}=${this.person.msg3&${this.input.msg4}=${this.person.msg4 || '無'}`;
         fetch(this.formAction + '?' + params, {
           method: 'POST'
         }).catch(err => {
@@ -53,8 +54,8 @@ const FormAutoFill = new Vue({
   },
   watch: {
     id: function(val) {
-      // ID 輸入到 4 碼就查詢資料
-      if(val.length === 4) {
+      // ID 輸入到 11 碼就查詢資料
+      if(val.length === 11) {
 
         // this.persons 裡沒這筆資料，才 POST
         if(this.persons[this.id] === undefined) {
